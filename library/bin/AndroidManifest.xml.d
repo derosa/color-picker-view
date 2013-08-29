f130392c8abba3528356415c8eef4901c44e63b1
derosa <soulnet@gmail.com>
2013-08-29 13:56:46 +0200

    Initial import

---
+++ library/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/david/git/color-picker-view-read-only/library/bin/AndroidManifest.xml : \
+/home/david/git/color-picker-view-read-only/library/AndroidManifest.xml \
---
+++ library/gen/afzkl/development/colorpickerview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package afzkl.development.colorpickerview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
